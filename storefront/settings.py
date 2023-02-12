c7a030a8a81aa121122471e3d1c6dce2f468ce7e
pratikbhadane24 <pratikbhadane24@gmail.com>
2023-02-12 20:59:06 +0530

    connected to db

--- storefront/settings.py
+++ storefront/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerceDB_effectnews',
+        'PORT': '3307',
+        'HOST': 'gmx.h.filess.io',
+        'USER': 'ecommerceDB_effectnews',
+        'PASSWORD': ''
     }
 }
 
