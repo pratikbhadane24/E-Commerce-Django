ebdaa13da52131ecdd9d04a38740d3ec694b631f
pratikbhadane24 <pratikbhadane24@gmail.com>
2023-02-14 22:15:37 +0530

    removed unrequired code

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, Customer, Collection, OrderItem
 
 # Create your views here.
 
 
 def say_hello(request):
-    queryset = OrderItem.objects.filter(product_id__collection__id=3)
-
-    return render(request, 'hello.html', {'fname': 'Naruto', 'lname': 'Uzumaki', 'products': list(queryset)})
+    return render(request, 'hello.html', {'fname': 'Naruto', 'lname': 'Uzumaki'})
