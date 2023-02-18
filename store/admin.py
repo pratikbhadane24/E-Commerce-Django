306cf88f0287b7134dd927ed9abb1946255c2e46
pratikbhadane24 <pratikbhadane24@gmail.com>
2023-02-18 13:31:18 +0530

    added inlines using generic relations

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from . import models
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.urls import reverse
 from django.db.models import Count
 from django.utils.html import format_html, urlencode
+from tags.models import TaggedItem
+from . import models
 # Register your models here.
 
 
@@ -33,6 +35,11 @@ class CollectionAdmin(admin.ModelAdmin):
         return super().get_queryset(request).annotate(product_count=Count('product'))
 
 
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ['tags']
+    model = TaggedItem
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     autocomplete_fields = ['collection']
@@ -40,6 +47,7 @@ class ProductAdmin(admin.ModelAdmin):
         'slug': ['title']
     }
     actions = ['clear_inventory']
+    inlines = [TagInline]
     list_display = ['title', 'unit_price',
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
