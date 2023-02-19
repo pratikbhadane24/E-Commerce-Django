5e1e7044739795042331e190f9a40c388d309021
pratikbhadane24 <pratikbhadane24@gmail.com>
2023-02-19 13:57:14 +0530

    added Model Serializers

--- store/serializers.py
+++ store/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from decimal import Decimal
+from store.models import Collection, Product
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(
-        max_digits=255, decimal_places=2, source='unit_price')
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
 
