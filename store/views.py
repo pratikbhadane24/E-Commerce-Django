3bc7b92febdfe7775b95f97684d4b9e40d8d60ee
pratikbhadane24 <pratikbhadane24@gmail.com>
2023-02-19 14:33:34 +0530

    moved post in product_list and not details

--- store/views.py
+++ store/views.py
@@ -4,28 +4,29 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
-# Create your views here.
-
-
-@api_view()
-def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(
-        queryset, many=True, context={'request': request})
-    return Response(serializer.data)
+# Create your views here.   
 
 
 @api_view(['GET', 'POST'])
-def product_details(request, id):
+def product_list(request):
     if request.method == 'GET':
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product)
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(
+            queryset, many=True, context={'request': request})
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.validated_data
+        serializer.save()
+        # serializer.validated_data
         return Response('OK')
+
+
+@api_view()
+def product_details(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
 
 
 @api_view()
